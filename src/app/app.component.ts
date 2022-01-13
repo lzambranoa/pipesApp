1d7ed78ebc8724dc82185a47e4f65d6526845841
lzamranoa <lzambranoa.software@gmail.com>
2022-01-13 18:55:13 -0500

    Modificacion del README.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'leonaRdo zambrano amezquita';
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
