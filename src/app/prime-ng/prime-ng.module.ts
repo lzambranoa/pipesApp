6d62dff59cb9e8219f18a9fccea1d0e8b64a9b16
lzamranoa <lzambranoa.software@gmail.com>
2022-01-17 13:09:00 -0500

    Instalacion y puesta a punto de primeNg

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+
+//NgPrime
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {MenubarModule} from 'primeng/menubar';
+
+
+
+@NgModule({
+  exports: [
+    ButtonModule,
+    CardModule,
+    MenubarModule
+  ]
+})
+export class PrimeNgModule { }
