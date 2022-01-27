c5bf163b026817777bf4aff868a0af1f5f455a10
lzamranoa <lzambranoa.software@gmail.com>
2022-01-26 22:01:14 -0500

    Cambio global del idioma a Español

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 
 @NgModule({
@@ -19,7 +24,9 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
